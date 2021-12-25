3f6025f706206d877d09b1112fc6d5e03f1d5c0b
unknown <kuchingneko28>
2021-12-25 18:23:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { url } = require("inspector");
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const url = "https://www.kominfo.go.id/content/all/berita_satker";
+  const { data } = await axios.get(url);
+
+  const $ = cheerio.load(data);
+  let artikel = [];
+  $(".blog-entry .content").each((index, element) => {
+    let data = {
+      thumbnail: "",
+      //   date: "",
+      title: "",
+      description: "",
+    };
+
+    //console.log($(element).children(".title").text());
+    data.thumbnail = $(element).children(".thumbnail-entry").children(".thumbnail-img").attr("src");
+    // data.date = $(element).children(".date h2").text();
+    data.title = $(element).children(".title").text();
+    data.description = $(element).children(".description").text();
+
+    artikel.push(data);
+  });
+
+  res.send(artikel);
+});
+
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
+
+// async function scrap() {
+//   try {
+//     const url = "https://www.kominfo.go.id/content/all/berita_satker";
+//     const { data } = await axios.get(url);
+
+//     const $ = cheerio.load(data);
+//     let artikel = [];
+//     $(".blog-entry .content").each((index, element) => {
+//       let data = {
+//         thumbnail: "",
+//         title: "",
+//         description: "",
+//       };
+
+//       //console.log($(element).children(".title").text());
+//       data.thumbnail = $(element).children(".thumbnail-entry").children(".thumbnail-img").attr("src");
+//       data.title = $(element).children(".title").text();
+//       data.description = $(element).children(".description").text();
+
+//       console.log(data);
+//       artikel.push(data);
+//     });
+
+//     console.log(artikel);
+
+//     return artikel;
+//   } catch (err) {
+//     if (err) {
+//       return "API ERROR";
+//     }
+//   }
+// }
