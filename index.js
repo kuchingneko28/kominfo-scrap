2a4083751734ee49fb38a7ca866c9f324354d202
unknown <kuchingneko28>
2021-12-26 22:09:03 +0800

    hadehh

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { send } = require("express/lib/response");
-
 const app = express();
+
 const port = process.env.PORT || 3000;
+const url = "https://www.kominfo.go.id/content/all/berita";
 
 app.get("/", (req, res) => {
-  res.send("Untuk mengakses api : http:///url/api");
+  res.send("Untuk mengakses : http:///url/api");
 });
 app.get("/api", async (req, res) => {
-  const url = "https://www.kominfo.go.id/content/all/berita_satker";
   const { data } = await axios.get(url);
 
   const $ = cheerio.load(data);
